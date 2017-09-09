60f239e7cff8849fe935dae83584a81089ba77c1
shakram02 <ahmedhamdyau@gmail.com>
2017-09-09 08:31:11 +0200

    format car class

--- lib/Car/Car.cpp
+++ lib/Car/Car.cpp
@@ -1,40 +1,33 @@
 #include "Car.h"
 
-  Car:: Car (int motor1For, int motor1Back,int motor2For, int motor2Back)
-  {
-   DC_Motor motorc1=  DC_Motor (motor1For,motor1Back) ;
-   DC_Motor motorc2=  DC_Motor(motor2For,motor2Back) ;
-   motor1 = motorc1;
-   motor2 = motorc2;
-  }
+Car::Car(int motor1For, int motor1Back, int motor2For, int motor2Back) {
+  DC_Motor motorc1 = DC_Motor(motor1For, motor1Back);
+  DC_Motor motorc2 = DC_Motor(motor2For, motor2Back);
+  motor1 = motorc1;
+  motor2 = motorc2;
+}
 
-  void Car:: setCarSpeed(int speed)
-  {
-    motor1.setSpeed(speed);
-    motor2.setSpeed(speed);
-  }
-  void Car:: forward()
-  {
-    motor1.forward();
-    motor2.forward();
-  }
-  void Car:: backward()
-  {
-    motor1.backward();
-    motor2.backward();
-  }
-  void Car:: right()
-  {
-    motor1.backward();
-    motor2.forward();
-  }
-  void Car:: left()
-  {
-    motor1.forward();
-    motor2.backward();
-  }
-  void Car:: stop()
-  {
-    motor1.stop();
-    motor2.stop();
-  }
+void Car::setCarSpeed(int speed) {
+  motor1.setSpeed(speed);
+  motor2.setSpeed(speed);
+}
+void Car::forward() {
+  motor1.forward();
+  motor2.forward();
+}
+void Car::backward() {
+  motor1.backward();
+  motor2.backward();
+}
+void Car::right() {
+  motor1.backward();
+  motor2.forward();
+}
+void Car::left() {
+  motor1.forward();
+  motor2.backward();
+}
+void Car::stop() {
+  motor1.stop();
+  motor2.stop();
+}
--- lib/Car/Car.h
+++ lib/Car/Car.h
@@ -1,14 +1,15 @@
 
 #ifndef CAR_H
-#define  CAR_H
-#include"DC_Motor.h"
+#define CAR_H
+#include "DC_Motor.h"
 class Car {
 private:
   int speed;
   DC_Motor motor1;
   DC_Motor motor2;
+
 public:
-  Car (int motor1For, int motor1Back,int motor2For, int motor2Back);
+  Car(int motor1For, int motor1Back, int motor2For, int motor2Back);
   void setCarSpeed(int speed);
   void forward();
   void backward();
